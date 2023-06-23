b72c3d504e27ba77b5c1b6fb373e5d1363bd185a
Chipatenii <innocentmanda70@gmail.com>
2023-06-22 21:24:20 -0700

    debuging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <main.h>
+
+
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
