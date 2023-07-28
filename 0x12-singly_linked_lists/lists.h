d3f5bb392a365c8dac5b2ebc5581e04531175ef5
Chipatenii <innocentmanda70@gmail.com>
2023-07-28 16:56:24 -0700

    fixing bugs

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -12,9 +11,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
